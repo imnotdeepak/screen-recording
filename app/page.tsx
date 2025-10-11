b2fa7bf4cd5e41609d77f59b7f1dffb40851c24b
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-10-11 12:29:44 -0400

    Base project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-karla">Page</h1>
+      Page
+    </div>
+  );
+};
+
+export default Page;
