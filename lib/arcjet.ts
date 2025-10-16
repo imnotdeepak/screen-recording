729eddf59af8927435a453bdfdfdc12ae16777b3
imnotdeepak <rambutanandlycheejuice@gmail.com>
2025-10-15 20:38:39 -0400

    Arcjet implementation to protect website

---
+++ lib/arcjet.ts
@@ -0,0 +1,29 @@
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  shield,
+  request,
+  validateEmail,
+  slidingWindow,
+  ArcjetDecision,
+  createMiddleware,
+} from "@arcjet/next";
+import { getEnv } from "./utils";
+
+export {
+  detectBot,
+  fixedWindow,
+  shield,
+  request,
+  slidingWindow,
+  validateEmail,
+  createMiddleware,
+  ArcjetDecision,
+};
+
+const aj = arcjet({
+  key: getEnv("ARCJET_API_KEY"),
+  rules: [],
+});
+
+export default aj;
